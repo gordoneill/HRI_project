9b1461042b1dfba16493ea57a7af95c51bb96615
Amanda Woolson <wamanda7@vt.edu>
2019-04-21 15:18:52 -0400

    Mods for trajectory planning. Needs to be tested! Now calculating joint angles only for 4 joints.

--- globals.m
+++ globals.m
@@ -1,23 +1,46 @@
 global tools;
 global commands;
 global robaiBot;
+global curPose;
+global mins;
+global maxs;
+global qhome;
 
-tools = ['wrench'; 'tweezers'; 'scalpel'; 'knife'; 'probe'];
+tools = ["wrench"; "tweezers"; "scalpel"; "knife"; "probe"];
 
-commands = ['up','down','left','right','forward','reverse',...
-    'rotateIn','rotateOut','rest','grip','release'];
+commands = ["up"; "down"; "left"; "right"; "forward"; "reverse";...
+    "rotateIn"; "rotateOut"; "rest"; "grip"; "release"];
 
-links = [     
-		  Revolute('d', 0.74, 'a', 0, 'alpha', pi/2)
-		  Revolute('d', 0, 'a', 1.0335, 'alpha', pi/2)
-		  Revolute('d', 0, 'a', 1.2583, 'alpha', pi/2)
-		  Revolute('d', 0, 'a', 1.1538, 'alpha', pi/2)
-		  Revolute('d', 0, 'a', 0.9752, 'alpha', pi/2)
-		  Revolute('d', 0, 'a', 0, 'alpha', pi/2) %0.7164
-		  Revolute('d', 0, 'a', 0, 'alpha', 0) %1.7252
+links = [   
+            Revolute('d', 0.17735, 'a', 0, 'alpha', pi/2) % 0.17735 = distance(L1-L0)
+            Revolute('d', 0, 'a', 0.24121, 'alpha', 0) % 0.24212 = distance(L3-L1)
+            Revolute('d', 0, 'a', 0.16916, 'alpha', 0) % 0.34168 = distance(end - L3)
+            Revolute('d', 0, 'a', 0.17252, 'alpha', 0)
+    
+% 		  Revolute('d', 0.74, 'a', 0, 'alpha', pi/2)
+% 		  Revolute('d', 0, 'a', 1.0335, 'alpha', pi/2)
+% 		  Revolute('d', 0, 'a', 1.2583, 'alpha', pi/2)
+% 		  Revolute('d', 0, 'a', 1.1538, 'alpha', pi/2)
+% 		  Revolute('d', 0, 'a', 0.9752, 'alpha', pi/2)
+% 		  Revolute('d', 0, 'a', 0.7164, 'alpha', -pi/2) %0.7164
+% 		  Revolute('d', 0, 'a', 0, 'alpha', pi) %1.7252
+          
         ];
 robaiBot =  SerialLink(links, 'name', 'Cyton Gamma 1500', 'manufacturer', 'Robai');
-%robaiBot.base = SE3(2, 1, 0);
-dest = robaiBot.ikine(SE3(2.31, 1.33, 0.0497)*SE3.Rx(-pi/6)*SE3.Rz(pi));
+robaiBot.base = transl(0,0,0.15); %*rpy2tr(0, 0, -pi/2);
 
-robaiBot.plot(dest)
+qz = [0 pi/2 0 0];% 0 0 0 0];
+% qhome = [0  pi/2 pi/3 pi/3]; % 0 0 0];
+deg = pi/180;
+
+curPose = SE3(0, 0.209, 0.1)*SE3.rpy(0,0,0,'deg');
+qhome = robaiBot.ikine(curPose, 'mask', [1 1 1 0 0 1]);
+
+mins = [  0 -15  -105 -105]*deg; 
+maxs = [180  90  105  105]*deg;
+
+robaiBot.plot(qhome, 'tilesize', 1)
+xlim([-1, 1])
+ylim([-1, 1])
+zlim([-1, 1])
+
--- move1D.m
+++ move1D.m
@@ -1,43 +1,114 @@
-function  traj = move1D(axis, pol, curPose)
+function   traj = move1D(axis, pol) %, curPose)
 %axis will contain x, y, or z (in the world frame
 %pol will contain the polarity to move (1 or -1)
 %curPose contains the current pose of the end effector 
 
 % Here we define the limits in the world frame of the arm
 global robaiBot; 
-
-xrange = [-10 10];
-yrange = [-10 10];
-zrange = [-10 10]; 
+global curPose;
+traj = [];
+xrange = [-0.390 0.390];
+yrange = [0 0.390];
+zrange = [0.02  0.1]; 
+radLims = [0.209 0.390];
+xyDist = 0.05; % 5 cm
+zDist = 0.08; %Must fluctuate between 0.02m and 0.1m
 
 switch(axis)
     case 'x'
-        translation = SE3(pol*2, 0, 0);
+        destX = curPose.t(1) + (pol*xyDist); 
+        rad = sqrt((pow(destX,2) + pow(curPose.t(2),2)));
+
+        if (destX < xrange(1))
+            dist = curPose.t(1) - xrange(1); 
+        elseif  (destX > xrange(2))
+            dist = xrange(2) - curPose.t(1); 
+        elseif (rad < radLims(1))
+            newX = sqrt(pow(radLims(1),2) - pow(curPose.t(2), 2));
+            dist = newX - curPose.t(1);
+        elseif (rad > radLims(2))
+            newX = sqrt(pow(radLims(2),2) - pow(curPose.t(2), 2));
+            dist = newX - curPose.t(1);
+        end 
+        translation = SE3(pol*dist, 0, 0);
     case 'y'
-        translation = SE3(0, pol*2, 0);        
+        destY = curPose.t(2) + (pol*xyDist); 
+        rad = sqrt((pow(destY,2) + pow(curPose.t(1),2)));
+
+        if (destY < yrange(1))
+            dist = curPose.t(2) - yrange(1); 
+        elseif  (destY > yrange(2))
+            dist = yrange(2) - curPose.t(2); 
+        elseif (rad < radLims(1))
+            newY = sqrt(pow(radLims(1),2) - pow(curPose.t(1), 2));
+            dist = newY - curPose.t(2);
+        elseif (rad > radLims(2))
+            newY = sqrt(pow(radLims(2),2) - pow(curPose.t(1), 2));
+            dist = newY - curPose.t(2);
+        end 
+        translation = SE3(0, pol*dist, 0);        
     case 'z'
-        translation = SE3(0, 0, pol*2);
+        if (((pol == 1) && (curPose.t(3) == zrange(1))) || ((pol == -1) && (curPose.t(3) == zrange(2))))
+            translation = SE3(0, 0, pol*zDist);  
+        else
+            if (pol == 1)
+               fprintf("Cannot go up. Already at up position.\n");
+            else
+               fprintf("Cannot go down. Already at down position.\n");
+            end
+            return;
+        end
+        
     otherwise
         disp("Invalid input.");
         return;
 end
 destPose = curPose * translation;
 
-% need to check that this is within bounds
-[ex, why, zee] = transl(destPose);
-if ((ex < xrange(1)) || (ex > xrange(2)) || ...
-    (why < yrange(1)) || (why > yrange(2)) || ...
-    (zee < zrange(1)) || (zee > zrange(2)))
-    disp("Out of range. Cannot move to desired location");
+start = robaiBot.ikine(curPose, 'mask', [1 1 1 0 0 1]);
+dest = robaiBot.ikine(destPose, 'mask', [1 1 1 0 0 1]);
+
+% Check joint angle ranges 
+for i=1:length(dest)
+    if (dest(i) > maxs(i))
+        fprintf("Joint angle %d exceeds limits. Is %d, cannot be greater than %d\n", i, dest(i), maxs(i));
+        if (i ~= length(dest))
+            dest(i+1) = dest(i+1) + (dest(i) - mins(i));
+        end
+        dest(i) = maxs(i);
+    elseif (dest(i) < mins(i))
+        fprintf("Joint angle %d exceeds limits. Is %d, cannot be less than %d\n", i, dest(i), mins(i));
+        if (i ~= length(dest))
+             dest(i+1) = dest(i+1) + (dest(i) - mins(i));
+        end
+        dest(i) = mins(i);
+    end
 end
 
-% create trajectory
-t = [0:0.1:4]';
+% Create the trajectory 
+st = 0.1;
+t = [0:st:4]';
 
-robaiBot.ikine();
-traj = curPose.interp(destPose, tpoly(0, 1, t));
+traj = start.interp(dest, tpoly(0, 1, t));
+robaiBot.plot(traj)
 
-tranimate(traj)
+for i=1:length(traj)
+     %Send joint angles to bot in steps
+     %Want each move to take ~4 seconds
+     %each step ~0.1 seconds (st) 
+    jointAngles = zeros(1,7);
+    movAngs = traj(i);
+    jointAngles(1:2) = movAngs(1:2);
+    jointAngles(4) = movAngs(3);
+    jointAngles(6) = movAngs(4);
+    success = robai.unity.putData(typecast(single(...
+              [rad2deg(jointAngles(1:7)), jointAngles(8)]), 'uint8')); 
+    if (~success)
+        disp("Failed to put data on robai");
+    end
+    sleep(st);
+    
+end
 
 end
 
